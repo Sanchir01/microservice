955d290176c85237a9ef0774568c053085151f0b
Sanchir01 <emgushovs@mail.ru>
2024-07-10 12:47:33 +0300

    create base config

---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int    `yaml:"port"`
+	Timeout string `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path != "" {
+		panic("config path not implemented")
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		log.Fatalf("CONFIG_PATH does not exist:%s", path)
+	}
+	var cfg Config
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		log.Fatalf("Failed to read config: %v", err)
+	}
+	return &cfg
+}
+func fetchConfigPath() string {
+	var res string
+	os.Getenv("CONFIG_PATH")
+	flag.StringVar(&res, "config", "", "config path file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+	return res
+}
